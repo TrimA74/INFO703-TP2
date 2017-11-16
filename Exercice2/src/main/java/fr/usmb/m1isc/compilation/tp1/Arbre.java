6920a3478c5d2a4f45fdb5de51bf0525a1b63bb7
TrimA <florianseb96@hotmail.fr>
2017-11-16 11:18:58 +0100

    exo1 done

---
+++ Exercice2/src/main/java/fr/usmb/m1isc/compilation/tp1/Arbre.java
@@ -0,0 +1,60 @@
+package fr.usmb.m1isc.compilation.tp1;
+
+/**
+ * Created by florian on 10/11/2017.
+ */
+public class Arbre {
+    private Arbre left;
+    private Arbre right;
+    private String value;
+    private Integer symbol;
+
+    public Arbre getLeft() {
+        return left;
+    }
+
+    public void setLeft(Arbre left) {
+        this.left = left;
+    }
+
+    public Arbre getRight() {
+        return right;
+    }
+
+    public void setRight(Arbre right) {
+        this.right = right;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Arbre(Arbre left, Arbre right, String value) {
+
+        this.left = left;
+        this.right = right;
+        this.value = value;
+    }
+    public static void parcoursProfondeur (Arbre a){
+        System.out.print("(");
+
+        System.out.print(a.value);
+        if(a.left != null){
+            parcoursProfondeur(a.left);
+        }
+        if(a.right != null){
+            parcoursProfondeur(a.right);
+        }
+        System.out.print(")");
+    }
+
+    @Override
+    public String toString() {
+        parcoursProfondeur(this);
+        return "";
+    }
+}
--- Exercice2/src/main/java/fr/usmb/m1isc/compilation/tp1/Main.java
+++ Exercice2/src/main/java/fr/usmb/m1isc/compilation/tp1/Main.java
@@ -1,19 +1,24 @@
 package fr.usmb.m1isc.compilation.tp1;
 
+
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java_cup.runtime.Symbol;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception  {
-		 LexicalAnalyzer yy;
-		 if (args.length > 0)
-		        yy = new LexicalAnalyzer(new FileReader(args[0])) ;
-		    else
-		        yy = new LexicalAnalyzer(new InputStreamReader(System.in)) ;
+		LexicalAnalyzer yy;
+		if (args.length > 0)
+			yy = new LexicalAnalyzer(new FileReader(args[0])) ;
+		else
+			yy = new LexicalAnalyzer(new InputStreamReader(System.in)) ;
 		@SuppressWarnings("deprecation")
 		parser p = new parser (yy);
-		p.parse( );
+
+		Symbol s = p.parse( );
+		Arbre a = (Arbre) s.value;
+		a.toString();
 	}
 
 }
